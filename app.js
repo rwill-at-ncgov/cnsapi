753ce53dcf0505a6948ea509541af2dc638ac706
rwill-at-ncgov <38567992+rwill-at-ncgov@users.noreply.github.com>
2018-10-26 12:37:58 -0400

    add files for config anf log

--- app.js
+++ app.js
@@ -3,6 +3,15 @@ const path = require('path')
 const cookieParser = require('cookie-parser')
 const bodyParser = require('body-parser')
 
+var expressWinston = require('express-winston');
+var winston = require('winston'); // for access log transports
+
+const log = require('./config/logging')
+//log.log.level = 'debug'; Set this in ../config/logging now 
+log.log.info('cnsapi starting');
+console.log('cnsapi starting');
+log.log.debug('Debugging enabled');
+
 const users = require('./routes/users')
 const version = require('./routes/version')
 
