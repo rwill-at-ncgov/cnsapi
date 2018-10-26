fa5b6e6026b0c683353f442b9382dd9f5a88ff4f
rwill-at-ncgov <38567992+rwill-at-ncgov@users.noreply.github.com>
2018-10-26 15:36:52 -0400

    mongodb connection test

--- app.js
+++ app.js
@@ -12,6 +12,11 @@ log.log.info('cnsapi starting');
 console.log('cnsapi starting');
 log.log.debug('Debugging enabled');
 
+const serverConfig = require('./config/local_serverConfig');
+
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
+
 const users = require('./routes/users')
 const version = require('./routes/version')
 
@@ -23,5 +28,26 @@ app.use(cookieParser())
 
 app.use('/cnsapi/v1/users', users)
 app.use('/cnsapi/v1/version', version)
+
+// =======================================================================
+
+// Connection URL
+const mdbConnectUrl = serverConfig.mdbUri;
+// Database Name
+const dbName = serverConfig.dbName;
+const dbclient = new MongoClient(mdbConnectUrl, { useNewUrlParser: true });
+
+// Use connect method to connect to the Server
+dbclient.connect(function(err) {
+	assert.equal(null, err);
+	log.log.info("connected to mongodb ${dbName} ok");
+	
+	const db = dbclient.db(dbName);
+
+  dbclient.close();
+  log.log.info("mongodb client connection closed");
+});
+
+// ==========================================================================
 
 module.exports = app
