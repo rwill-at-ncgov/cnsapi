3b54e1b4de50c633c11f7b9f149fb447ac615956
rwill-at-ncgov <38567992+rwill-at-ncgov@users.noreply.github.com>
2018-10-26 14:39:55 -0400

    update packages and support winston v3

--- config/logging.js
+++ config/logging.js
@@ -2,20 +2,56 @@
 	Configure logging for app
 */
 
-const applog = require('winston');
+//const applog = require('winston');
+// for v3
+const { createLogger, format, transports } = require('winston');
 const tsFormat = () => (new Date()).toString();
+const logform = require('logform');
+const { combine, timestamp, label, printf } = logform.format;
+
+/*
 const logger = new (applog.Logger)({
   transports: [
-	/*
+	
     new (applog.transports.Console)({ 
 		timestamp: tsFormat,
 		colorize: true,
 	}),
-	*/
+
 	new (applog.transports.File)({
       filename: `cnsapi.log`,
       timestamp: tsFormat,
 	  colorize: true,
+    })
+  ],
+  level: "debug"
+});
+*/
+
+// for v3
+const logger = createLogger({
+	format: format.combine(
+		//timestamp(),
+		label({ label: '[cnsapi]' }),
+        timestamp({
+            format: 'YYYY-MM-DD HH:mm:ss'
+		}),
+		printf(nfo => {
+			return `[${nfo.label}] ${nfo.level}: ${nfo.message}`;
+		}),
+		format.json()
+	),
+	transports: [
+	/*
+	new transports.Console({
+		timestamp: tsFormat,
+		colorize: true
+	}),
+	*/
+	new (transports.File)({
+      filename: `cnsapi.log`,
+      //timestamp: tsFormat,
+	  //colorize: true,
     })
   ],
   level: "debug"
